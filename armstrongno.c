3fd590c2ff9e10d21f00d0e9ecce65257011636a
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-06 22:54:26 +0530

    Add files via upload

---
+++ armstrongno.c
@@ -0,0 +1,29 @@
+//Armstrong number
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int remainder,originalnum,num,sum=0;
+printf("Enter a number: ");
+scanf("%d",&num);
+originalnum=num;
+while(num>0)
+{
+    remainder = num % 10;
+    sum = sum + pow(remainder,3);
+    num = num / 10;
+
+}
+if(originalnum == sum)
+{
+    printf("\n%d is an Armstrong number.",originalnum);
+}
+else
+{
+    printf("\n%d is NOT an armstrong number");
+}
+
+return 0;
+
+}
