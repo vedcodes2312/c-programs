a37cacb66b908aa11dc8fa9c3b77065578a77a48
Ved Vyas <vedvyascodes1608@gmail.com>
2024-11-13 21:25:45 +0530

    Create factorialrecursion.c

---
+++ recursion/factorialrecursion.c
@@ -0,0 +1,26 @@
+//factorial using recursion
+#include <stdio.h>
+
+int factorial(int n); //prototype
+
+int main()
+{
+    int num;
+    printf("enter value of num\n");
+    scanf("%d",&num);
+    int result = factorial(num);  //call
+    printf("%d factorial is %d",num,result);
+    return 0;
+}
+
+int factorial(int n) //recursive function
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n-1);
+    }
+}
