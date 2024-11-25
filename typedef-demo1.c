30a0ab95ae919b0fe85dd484c85b854e81b39c8e
Ved Vyas <vedvyascodes1608@gmail.com>
2024-11-25 23:54:36 +0530

    Add files via upload

---
+++ typedef-demo1.c
@@ -0,0 +1,18 @@
+//demonstration of typedef
+//github : vedcodes2312
+
+#include<stdio.h> //preprocessor directive to include the standard input/output header file
+
+typedef int INTEGER; //create an alias for int
+
+int main(void) //body of the program
+{
+
+INTEGER num = 3; //assignment of value
+
+printf("%d", num); //print the value
+
+return 0;
+
+}        //end of code
+
