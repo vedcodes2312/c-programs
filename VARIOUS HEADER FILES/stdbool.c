7d71b913d85f78f410a9ee5f00b91da44f24b212
Ved Vyas <vedvyascodes1608@gmail.com>
2024-12-04 23:32:41 +0530

    Add files via upload

---
+++ VARIOUS HEADER FILES/stdbool.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+int main()
+{
+    system("cls");
+   // Declaration of boolean data types 
+   bool x = true;
+   bool y = false;
+
+   printf("True : %d\n", x);
+   printf("False : %d", y);
+
+   return 0;
+}
